b2041dbc7395e29a48a49d43eb5207f547ad2138
sergiohut <110191929+sergiohut@users.noreply.github.com>
2024-02-22 19:27:51 +0100

    Create recetas_tradicionales.py

---
+++ models/recetas_tradicionales.py
@@ -0,0 +1,42 @@
+from enum import Enum, auto
+
+#aqui se genera un enum con los ingredientes de la cocina tradicional española
+class IngredientesTradicionales(Enum):
+    ACEITE_DE_OLIVA = auto()
+    ACEITUNAS = auto()
+    AJO = auto()
+    ALCACHOFAS = auto()
+    ALCAPARRAS = auto()
+    ARROZ = auto()
+    AZAFRÁN = auto()
+    CALABACINES = auto()
+    CALAMARES = auto()
+    CEBOLLA = auto()
+    COMINO = auto()
+    CONEJO = auto()
+    ESPÁRRAGOS = auto()
+    GAMBAS = auto()
+    HUEVOS = auto()
+    JAMÓN = auto()
+    JUDIAS_VERDES = auto()
+    LAUREL = auto()
+    LENTEJAS = auto()
+    LIMONES = auto()
+    OREGANO = auto()
+    PAN = auto()
+    PATATAS = auto()
+    PESCADO = auto()
+    PIMENTA = auto()
+    PIMENTON_VERA = auto()
+    PIMIENTO_CHORICERO = auto()
+    PIMIENTO_VERDE = auto()
+    PIMIENTOS_DE_PADRÓN = auto()
+    PIMIENTOS_ROJOS = auto()
+    POLLO = auto()
+    ROMERO = auto()
+    TOMATE = auto()
+    TOMILLO = auto()
+
+
+
+
