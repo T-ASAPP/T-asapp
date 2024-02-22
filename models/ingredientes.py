4a4723a2bc8c2cffe59a608a0e34c638c8833073
Avz20 <104647270+Avz20@users.noreply.github.com>
2024-02-22 20:34:11 +0100

    Create ingredientes.py

---
+++ models/ingredientes.py
@@ -0,0 +1,21 @@
+# Sets para cada tipo de ingrediente
+carnes = {"Pollo", "Res", "Cerdo", "Pescado"}
+vegetales = {"Cebolla", "Pimiento", "Tomate", "Zanahoria"}
+frutas = {"Manzana", "Plátano", "Naranja", "Fresa"}
+granos = {"Arroz", "Pasta", "Trigo", "Avena"}
+lacteos = {"Leche", "Queso", "Yogur", "Mantequilla"}
+hierbas = {"Perejil", "Cilantro", "Albahaca", "Menta"}
+especias = {"Canela", "Nuez moscada", "Clavo", "Jengibre", "Sal", "Pimienta negra", "Pimentón", "Cúrcuma", "Comino", "Orégano", "Albahaca"}
+condimentos = {"Sal", "Ajo en Polvo"}
+
+# Diccionario a partir de los sets
+ingredientes = {
+    "Carnes": carnes,
+    "Vegetales": vegetales,
+    "Frutas": frutas,
+    "Granos": granos,
+    "Lácteos": lacteos,
+    "Hierbas": hierbas,
+    "Especias": especias,
+    "Condimentos": condimentos,
+}
