8072547ef359881efc33cb904346be2a03d9f3fc
tomasjpardo <152438816+tomasjpardo@users.noreply.github.com>
2024-02-26 20:03:16 +0100

    Update unidades.py
    se ha creado una biblioteca con las posibles medidas para usar en una receta de cocina.

--- Recipeers/unidades.py
+++ Recipeers/unidades.py
@@ -1,1 +1,6 @@
-unidades_medida = {"l", "kg", "g"}
+medidas_recetas = {
+    "solidos": {"kg", "g"},  # Unidades de medida para sólidos
+    "liquidos": {"l", "ml"},  # Unidades de medida para líquidos
+    "otros": {"cda", "cdta", "tazas", "pizcas"}  # Otras unidades de medida comunes en recetas
+}
+
