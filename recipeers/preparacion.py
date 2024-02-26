1be8b3a776462cae2ceae6b420c59dc4478ac270
Andrea <158600488+AndreaMGG@users.noreply.github.com>
2024-02-26 19:49:19 +0100

    edicion codigo issue5

---
+++ recipeers/preparacion.py
@@ -0,0 +1,7 @@
+# Definir conjunto con verbos frecuentes de cocinado
+verbo_cocinado = {"picar", "cortar", "cocinar", "freír", "hervir", "mezclar", "saltear", "hornear", "agregar", "batir", "añadir", "cocer", "pochar", "sofreir"}
+# Definir variables instrucciones e ingredientes
+mas_instrucciones = str
+ingredientes = str
+# Concatenar todo    
+instrucciones = f"{verbo_cocinado} {ingredientes} {mas_instrucciones}"
