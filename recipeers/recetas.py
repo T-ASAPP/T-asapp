1503dbd25124eaaeb1dc4f2cb2726ff8c90c7902
sergiohut <sergiosantiagocarmona@gmail.com>
2024-02-27 19:13:50 +0100

    #37 listaIngredientes pasa a ser una tupla para incluir cantidades

--- recipeers/recetas.py
+++ recipeers/recetas.py
@@ -1,5 +1,5 @@
 class Receta:
-    def __init__(self, nombre: str, listaIngredientes: List[Ingrediente], pasosPreparacion: List[Preparacion]):
+    def __init__(self, nombre: str, listaIngredientes: List[Tuple[Ingrediente, float]], pasosPreparacion: List[Preparacion]):
         self.nombre = nombre
         self.listaIngredientes = listaIngredientes
         self.pasosPreparacion = pasosPreparacion
