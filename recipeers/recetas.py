3b9dace35bd26db4861365148121b38887ded46f
sergiohut <sergiosantiagocarmona@gmail.com>
2024-02-27 19:34:51 +0100

    #37 incluimos definición de las clases involucradas en clase receta

--- recipeers/recetas.py
+++ recipeers/recetas.py
@@ -3,3 +3,15 @@ class Receta:
         self.nombre = nombre
         self.listaIngredientes = listaIngredientes
         self.pasosPreparacion = pasosPreparacion
+
+class Ingrediente:
+  def __init__(self, nombre: set[str], medida: str):
+    self.nombre = nombre
+    self.medida = medida   
+
+class Preparacion:
+    def __init__(self, verbos_cocinado: set[str], ingredientes: str, instrucciones: str):
+
+        self.verbos_cocinado = verbos_cocinado
+        self.ingredientes = ingredientes
+        self.instrucciones = instrucciones
