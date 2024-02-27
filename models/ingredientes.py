06db7e0e496716ff4db3a09b31e2356769640a0b
Avz20 <104647270+Avz20@users.noreply.github.com>
2024-02-27 13:05:27 +0100

    Update ingredientes.py

--- models/ingredientes.py
+++ models/ingredientes.py
@@ -1,19 +1,7 @@
-# Sets para cada tipo de ingrediente
-carnes = {"Pollo", "Res", "Cerdo", "Pescado"}
-vegetales = {"Cebolla", "Pimiento", "Tomate", "Zanahoria"}
-frutas = {"Manzana", "Plátano", "Naranja", "Fresa"}
-granos = {"Arroz", "Pasta", "Trigo", "Avena"}
-lacteos = {"Leche", "Queso", "Yogur", "Mantequilla"}
-condimentos = {"Sal", "Ajo en Polvo", "Canela", "Nuez moscada", "Jengibre", "Pimienta negra", "Pimentón", "Cúrcuma", "Comino", "Orégano", "Perejil", "Cilantro", "Albahaca", "Menta"}
+class Ingredientes:
 
-# Diccionario a partir de los sets
-ingredientes = {
-    "Carnes": carnes,
-    "Vegetales": vegetales,
-    "Frutas": frutas,
-    "Granos": granos,
-    "Lácteos": lacteos,
-    "Hierbas": hierbas,
-    "Especias": especias,
-    "Condimentos": condimentos,
-}
+  def __init__(self, nombre: set[str], medida: str, cantidad: float):
+
+    self.nombre = nombre
+    self.medida = medida
+    self.cantidad = cantidad
