200eb1ec373860482536aeb741d6346f2c09a4ae
sergiohut <sergiosantiagocarmona@gmail.com>
2024-02-27 19:51:58 +0100

    #37 Incluimos las importaciones de las clases necesarias, incluidas List y Tuple

--- recipeers/recetas.py
+++ recipeers/recetas.py
@@ -1,17 +1,10 @@
+from typing import List, Tuple
+from preparacion import Preparacion
+from ingrediente import Ingrediente
+
 class Receta:
-    def __init__(self, nombre: str, listaIngredientes: List[Tuple[Ingrediente, float]], pasosPreparacion: List[Preparacion]):
+    def __init__(self, nombre: str, 
+                 listaIngredientes: List[Tuple[Ingrediente, float]], pasosPreparacion: List[Preparacion]):
         self.nombre = nombre
         self.listaIngredientes = listaIngredientes
         self.pasosPreparacion = pasosPreparacion
-
-class Ingrediente:
-  def __init__(self, nombre: set[str], medida: str):
-    self.nombre = nombre
-    self.medida = medida   
-
-class Preparacion:
-    def __init__(self, verbos_cocinado: set[str], ingredientes: str, instrucciones: str):
-
-        self.verbos_cocinado = verbos_cocinado
-        self.ingredientes = ingredientes
-        self.instrucciones = instrucciones
