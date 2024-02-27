3c1bd060f6490030390d11d9bd158f855b2dd403
tomasjpardo <tomasjpardo@yahoo.es>
2024-02-27 17:04:01 +0100

    #43 #45. Crea biblioteca con medidas para recetas

--- Recipeers/unidades.py
+++ Recipeers/unidades.py
@@ -1,1 +1,5 @@
-unidades_medida = {"l", "kg", "g"}
+medida = {
+    "solidos": {"kg", "g"},  # Unidades de medida para sólidos
+    "liquidos": {"l", "ml"},  # Unidades de medida para líquidos
+    "otros": {"cda", "cdta", "tazas", "pizcas", "unidad"}  # Otras unidades de medida comunes en recetas
+}
