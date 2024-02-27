7e2afba569ae3fc56149eb6d0e6749ee8db54aa5
Andrea <158600488+AndreaMGG@users.noreply.github.com>
2024-02-27 19:25:38 +0100

    #35 UpdatCodigo Ingredientes cambio 'cantidad' a #44

--- recipeers/ingredientes.py
+++ recipeers/ingredientes.py
@@ -1,15 +1,4 @@
-class Ingrediente:
-    def __init__(self, nombre: str):
-        self.nombre = nombre
-
-    def __repr__(self):
-        return f"Ingrediente({self.nombre})"
-    
-class CantidadIngrediente:
-    def __init__(self, ingrediente: Ingrediente, cantidad: float, unidad_medida: str):
-        self.ingrediente = ingrediente
-        self.cantidad = cantidad
-        self.unidad_medida = unidad_medida
-
-    def __repr__(self):
-        return f"CantidadIngrediente({self.ingrediente}, {self.cantidad}, {self.unidad_medida})"    
+class Ingredientes:
+  def __init__(self, nombre: set[str], medida: str):
+    self.nombre = nombre
+    self.medida = medida    
