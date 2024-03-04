37cbca1689643b0703bdf61ff6f6c06dbd97c174
tomasjpardo <tomasjpardo@yahoo.es>
2024-03-04 16:36:27 +0100

    #43 #45 Cambio a minúsculas de carpeta y se agregan más medidas para receta

--- Recipeers/unidades.py
+++ Recipeers/unidades.py
@@ -7,4 +7,7 @@ medida = {
     "cucharada",  # Cucharadas
     "cucharadita",  # Cucharaditas
     "pizca"  # Pizcas para cantidades muy pequeñas
+    "pellizco" # Equivalente a la cantidad que se puede coger con el dedo pulgar e índice
+    "vaso de yogurt" # En algunas recetas de repostería se utiliza como medida, según la marca equivale a 125 ml aprox
+    "al gusto" # Generalmente se usa para sazonar, imperando el criterio de quien realiza la receta
 }
