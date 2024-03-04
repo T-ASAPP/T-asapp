a7bedf91a43f20c185691cb2354b12b9777696db
tomasjpardo <tomasjpardo@yahoo.es>
2024-03-04 16:44:45 +0100

    #43 #45 Se corrigen comas en el set

--- Recipeers/unidades.py
+++ Recipeers/unidades.py
@@ -6,8 +6,8 @@ medida = {
     "unidades",  # Conteo de elementos individuales
     "cucharada",  # Cucharadas
     "cucharadita",  # Cucharaditas
-    "pizca"  # Pizcas para cantidades muy pequeñas
-    "pellizco" # Equivalente a la cantidad que se puede coger con el dedo pulgar e índice
-    "vaso de yogurt" # En algunas recetas de repostería se utiliza como medida, según la marca equivale a 125 ml aprox
+    "pizca",  # Pizcas para cantidades muy pequeñas
+    "pellizco", # Equivalente a la cantidad que se puede coger con el dedo pulgar e índice
+    "vaso de yogurt", # En algunas recetas de repostería se utiliza como medida, según la marca equivale a 125 ml aprox
     "al gusto" # Generalmente se usa para sazonar, imperando el criterio de quien realiza la receta
 }
