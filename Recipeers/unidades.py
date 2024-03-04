f2b0d858e7e855709e957d03523310ee636f73f6
tomasjpardo <tomasjpardo@yahoo.es>
2024-03-04 16:56:43 +0100

    #43 #45. Unificación medidas a plural

--- Recipeers/unidades.py
+++ Recipeers/unidades.py
@@ -4,10 +4,10 @@ medida = {
     "l",   # Litros
     "ml",  # Mililitros
     "unidades",  # Conteo de elementos individuales
-    "cucharada",  # Cucharadas
-    "cucharadita",  # Cucharaditas
-    "pizca",  # Pizcas para cantidades muy pequeñas
-    "pellizco", # Equivalente a la cantidad que se puede coger con el dedo pulgar e índice
-    "vaso de yogurt", # En algunas recetas de repostería se utiliza como medida, según la marca equivale a 125 ml aprox
+    "cucharadas",  # Cucharadas
+    "cucharaditas",  # Cucharaditas
+    "pizcas",  # Pizcas para cantidades muy pequeñas
+    "pellizcos", # Equivalente a la cantidad que se puede coger con el dedo pulgar e índice
+    "vasos de yogurt", # En algunas recetas de repostería se utiliza como medida, según la marca equivale a 125 ml aprox
     "al gusto" # Generalmente se usa para sazonar, imperando el criterio de quien realiza la receta
 }
