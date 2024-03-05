bcec64ffcd0cfff406cdfa66cf211a3a61a2f072
Avz20 <vz.andrea20@gmail.com>
2024-03-05 20:30:00 +0100

    #51 set eliminado

--- recipeers/ingrediente.py
+++ recipeers/ingrediente.py
@@ -1,6 +1,6 @@
 from medida import medida
 
 class Ingrediente:
-  def __init__(self, nombre: set[str], medida: str):
+  def __init__(self, nombre: str, medida: str):
     self.nombre = nombre
     self.medida = medida
