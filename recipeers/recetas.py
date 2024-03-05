31b495b877d10e9fdbdd5786be76231c5bc55e91
maguilerafr <lolo.aguilera.98@gmail.com>
2024-03-05 20:41:41 +0100

    Modificación de recetas
    Issue #52. La receta es un objeto valor que se forma con la relación de preparacion.py e ingrediente.py. Se han eliminado las funciones porque en este punto del proyecto no es necesario establecer las mismas, solamente las instancias entre estas

--- recipeers/recetas.py
+++ recipeers/recetas.py
@@ -1,23 +1,11 @@
 from typing import List, Tuple
 from preparacion import Preparacion
 from ingrediente import Ingrediente
-from tipo_receta import TipoReceta
 
 class Receta:
     def __init__(self, nombre: str, 
                  lista_ingredientes: List[Tuple[Ingrediente, float]] = [], 
-                 pasos_preparacion: List[Preparacion] = [], 
-                 tipo: TipoReceta = None):
+                 pasos_preparacion: List[Preparacion] = []):
         self.nombre = nombre
         self.lista_ingredientes = lista_ingredientes
         self.pasos_preparacion = pasos_preparacion
-        self.tipo = tipo
-    
-    def agregar_ingrediente(self, ingrediente: Ingrediente, cantidad: float):
-        self.lista_ingredientes.append((ingrediente, cantidad))
-    
-    def agregar_paso_preparacion(self, paso: Preparacion):
-        self.pasos_preparacion.append(paso)
-    
-    def asignar_tipo(self, tipo: TipoReceta):
-        self.tipo = tipo
