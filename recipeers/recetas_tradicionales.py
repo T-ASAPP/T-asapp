411f8bb4bc164360532c5feec4da067c2ddbbd27
Andrea <158600488+AndreaMGG@users.noreply.github.com>
2024-03-06 20:01:44 +0100

    update cod recetas,rtradicionales,ingredi
    He cambiado el codigo de los archivos recetas.py,recetas__tradicionales.py e ingrediente.py en cuanto a nombre de las clases y variables para que sigan la convecion snakecase ya que trabajamos en python

--- recipeers/recetas_tradicionales.py
+++ recipeers/recetas_tradicionales.py
@@ -1,6 +1,6 @@
-class TipoReceta:
+class Tipo_Receta:
     def __init__(self, nombre: str):
         self.nombre = nombre
 
-# Crear instancias de TipoReceta
-tradicional = TipoReceta("Tradicional")
+# Crear instancias de Tipo_Receta
+tradicional = Tipo_Receta("Tradicional")
