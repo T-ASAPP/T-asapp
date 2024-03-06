838292a7d166fc1d1349e14faf2b47202307ba06
Andrea <158600488+AndreaMGG@users.noreply.github.com>
2024-03-06 20:25:12 +0100

    update codigo tipo_receta.py
    para corregir un error en el codigo de este fichero en cuanto al nombre de la clase para que siga la convencion snakecase en la que estamos trabajando en python

--- recipeers/tipo_receta.py
+++ recipeers/tipo_receta.py
@@ -1,6 +1,6 @@
-class tipo_receta:
+class Tipo_receta:
     def __init__(self, nombre: str):
         self.nombre = nombre
 
 # Crear instancias de Tipo_Receta
-tradicional = tipo_receta("Tradicional")
+tradicional = Tipo_receta("Tradicional")
