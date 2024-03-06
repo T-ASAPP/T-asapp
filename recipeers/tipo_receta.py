46ccd360ac858ed24cc9d7e874dd02e4a5730e5b
maguilerafr <lolo.aguilera.98@gmail.com>
2024-03-06 20:45:28 +0100

    Adición de los Docstring de tipo receta. Issue #65

--- recipeers/tipo_receta.py
+++ recipeers/tipo_receta.py
@@ -1,6 +1,14 @@
-class TipoReceta:
-    def __init__(self, nombre: str):
-        self.nombre = nombre
+class tipo_receta:
+    """Clase que representa un tipo de receta.
 
-# Crear instancias de TipoReceta
-tradicional = TipoReceta("Tradicional")
+    Attributes:
+        nombre (str): El nombre del tipo de receta.
+    """
+
+    def __init__(self, nombre: str):
+        """Inicializa una instancia de la clase tipo_receta.
+
+        Args:
+            nombre (str): El nombre del tipo de receta.
+        """
+        self.nombre = nombre
