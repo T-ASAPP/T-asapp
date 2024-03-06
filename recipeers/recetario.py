a65a4b86a9631c17b10bfc3d7756c9fde47d0638
AndreaMGG <158600488+AndreaMGG@users.noreply.github.com>
2024-03-06 17:46:19 +0100

    Borro recetario.py x no seguir buenas practicas commit
    Elimino el archivo recetario.py porque el mensaje de commit no seguía las buenas prácticas.

--- recipeers/recetario.py
+++
@@ -1,9 +0,0 @@
-from typing import List, Dict
-
-from recetas import Receta
-from recetas_tradicionales import TipoReceta
-
-class Recetario:
-
-  def __init__(self):
-    self._recetas: Dict[str, Receta] = {}
