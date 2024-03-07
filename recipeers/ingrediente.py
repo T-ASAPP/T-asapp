c6e98f1ae6d92facf1165435055f232c0f2e06d4
maguilerafr <lolo.aguilera.98@gmail.com>
2024-03-07 16:26:26 +0100

    Adición de los Docstring en ingrediente. Issue #65

--- recipeers/ingrediente.py
+++ recipeers/ingrediente.py
@@ -1,6 +1,24 @@
 from medida import Medida
 
 class Ingrediente:
-  def __init__(self, nombre: str, medida: Medida):
-    self.nombre = nombre
-    self.medida = medida
+    """Clase que representa un ingrediente utilizado en una receta.
+
+    Contiene información sobre el nombre del ingrediente y la medida en la que se utiliza.
+
+    Attributes:
+        nombre (str): El nombre del ingrediente.
+        medida (Medida): El objeto de la clase Medida que representa la unidad de medida
+            en la que se utiliza el ingrediente.
+
+    """
+
+    def __init__(self, nombre: str, medida: Medida):
+        """Inicializa un nuevo ingrediente con su nombre y medida.
+
+        Args:
+            nombre (str): El nombre del ingrediente.
+            medida (Medida): El objeto de la clase Medida que representa la unidad de medida
+                en la que se utiliza el ingrediente.
+        """
+        self.nombre = nombre
+        self.medida = medida
