de6664fa8fd5b98afbafcb513518802eb491fe04
maguilerafr <lolo.aguilera.98@gmail.com>
2024-03-07 16:23:03 +0100

    Adición de los Docstring en recetario. Issue #65

--- recipeers/recetario.py
+++ recipeers/recetario.py
@@ -1,8 +1,21 @@
-from typing import List, Dict
-
+from typing import Dict
 from receta import Receta
 from tipo_receta import Tipo_receta
 
 class Recetario:
- def __init__(self):
-   self._recetas: Dict[str, Receta] = {}
+    """Clase que representa un recetario que contiene recetas.
+
+    Un recetario es una colección de recetas organizadas por un identificador único. 
+    Permite realizar operaciones como añadir nuevas recetas, eliminar recetas existentes,
+    y buscar recetas por su identificador.
+
+    Attributes:
+        _recetas (Dict[str, Receta]): Un diccionario que almacena las recetas del recetario,
+            donde las claves son los identificadores únicos de las recetas y los valores son
+            objetos de la clase Receta.
+
+    """
+
+    def __init__(self):
+        """Inicializa un nuevo recetario sin recetas."""
+        self._recetas: Dict[str, Receta] = {}
