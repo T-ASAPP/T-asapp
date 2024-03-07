1beeaf9968b963d14e5216152a06ce7478347cb1
Paul Koder <158593523+PabloES796@users.noreply.github.com>
2024-03-07 16:19:25 +0100

    Update medida.py
    Se cambia yogurt por yogur, ya que a la hora de hacer una búsqueda simplifica y tambien sirve para ambos.

--- recipeers/medida.py
+++ recipeers/medida.py
@@ -5,12 +5,12 @@ class Medida:
             "g": "Gramo",
             "l": "Litro",
             "ml": "Mililitro",
-            "unidad": "Conteo de elemento individuale",
+            "unidad": "Conteo de elemento individual",
             "cucharada": "Cuchara sopera",
             "cucharadita": "Cuchara pequeña",
             "pizca": "Para cantidades muy pequeñas",
             "pellizco": "Equivalente a la cantidad que se puede coger con el dedo pulgar e índice",
-            "vaso_de_yogurt": "Vaso de yogurt (125 ml aprox)",
+            "vaso_de_yogur": "Vaso de yogur (125 ml aprox)",
             "al_gusto": "Al gusto (sazonar)"
         }
 
